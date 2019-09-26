516769955942d48dd437158e656615554d2c804c
Slav666 <slawomirdyk@gmail.com>
2019-09-26 11:09:52 +0100

    WebServlet configured, jetty server successfully run on port 8080

--- src/main/java/io/github/mat3e/App.java
+++ src/main/java/io/github/mat3e/App.java
@@ -3,12 +3,15 @@ package io.github.mat3e;
 
 import org.slf4j.LoggerFactory;
 
+;
+
 /**
  * Hello world!
  */
 public class App {
-    public static void main(String[] args) {
-        var logger = LoggerFactory.getLogger(App.class);
-        logger.info("Hello World!");
+    public static void main(String[] args) throws Exception{
+    var logger = LoggerFactory.getLogger(App.class);
+    logger.info("Hello World");
+
     }
 }
